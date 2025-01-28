78f0896f132973a0293c0d94eded94c0b4a7c867
WidaSuryani <widasuryani1@gmail.com>
2025-01-28 18:04:04 +0800

    fix : update icon and also styling

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,14 +1,15 @@
 const HomePage = () => {
   return (
-    <div className="mx-2">
-      <h1 className="text-2xl font-bold">Post Page</h1>
-      <div>
-        <div className="flex">
-          <span className="i-noto-dolphin" />
-          <p>Title</p>
-        </div>
-        <div>
-          <p>Content</p>
+    <div className="bg-gradient-to-r from-[#8eaad4] to-[#ddb2bc]">
+      <div className="mx-2">
+        <h1 className="text-2xl font-bold py-2">Post Page</h1>
+        <div className="border border-indigo-600 rounded-lg px-2 w-3/4">
+          <div className="flex">
+            <p className="font-bold text-xl">Title</p>
+          </div>
+          <div>
+            <p>Content</p>
+          </div>
         </div>
       </div>
     </div>
