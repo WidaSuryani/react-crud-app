f4a92d9c2ecd53d348a5393a1e766caf6c7dabd9
WidaSuryani <widasuryani1@gmail.com>
2025-01-29 16:06:53 +0800

    fix : update design

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,17 +1,54 @@
+import { useEffect, useState } from "react";
+
+interface Post {
+  id: number;
+  title: string;
+  content: string;
+}
+
 const HomePage = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/posts")
+      .then((response) => response.json())
+      .then((data) => setPosts(data));
+  }, []);
+
+  const handleDelete = (id: number) => {
+    fetch(`http://localhost:3001/posts/${id}`, {
+      method: "DELETE",
+    }).then(() => setPosts(posts.filter((post) => post.id !== id)));
+  };
+
   return (
-    <div className="bg-gradient-to-r from-[#8eaad4] to-[#ddb2bc]">
-      <div className="mx-2">
-        <h1 className="text-2xl font-bold py-2">Post Page</h1>
-        <div className="border border-indigo-600 rounded-lg px-2 w-3/4">
-          <div className="flex">
-            <p className="font-bold text-xl">Title</p>
+    <div className="min-h-screen flex bg-gradient-to-r from-[#8eaad4] to-[#ddb2bc] px-2">
+      <div>
+        <h1 className="text-2xl font-bold py-2 text-center block">Post Page</h1>
+        {posts.map((post) => (
+          <div
+            key={post.id}
+            className="border border-white rounded-lg px-2 py-2 my-2 shadow-xl"
+          >
+            <h2 className="font-bold text-xl">{post.title}</h2>
+            <p>{post.content}</p>
+            <div className="flex py-2">
+              <button className="px-4 py-2 bg-amber-200 flex items-center rounded hover:bg-amber-300 ">
+                Edit <span className="i-mdi-file-edit" />
+              </button>
+              <button
+                className="ml-2 px-4 py-2 bg-red-200 flex items-center rounded hover:bg-red-400 "
+                onClick={() => handleDelete(post.id)}
+              >
+                Delete <span className="i-mdi-delete" />
+              </button>
+            </div>
           </div>
-          <div>
-            <p>Content</p>
-          </div>
-        </div>
+        ))}
       </div>
+      <button className="fixed bottom-4 right-4 bg-blue-200 px-4 py-2 flex items-center rounded hover:bg-blue-400 ">
+        Create Post <span className="i-mdi-add-box" />
+      </button>
     </div>
   );
 };
