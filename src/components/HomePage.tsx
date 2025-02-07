0f4c82b81b74f84a27aec2abd0b989697b69ae1a
WidaSuryani <widasuryani1@gmail.com>
2025-02-07 18:44:38 +0700

    fix : update style

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -2,11 +2,11 @@ import { useQuery } from "@tanstack/react-query";
 // import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-// interface Post {
-//   id: number;
-//   title: string;
-//   content: string;
-// } // to define the type of the data that we are going to use.
+interface Post {
+  id: number;
+  title: string;
+  content: string;
+} // to define the type of the data that we are going to use.
 
 const HomePage = () => {
   // const [posts, setPosts] = useState<Post[]>([]); // useState is using to manage form inputs and local state.
@@ -14,12 +14,14 @@ const HomePage = () => {
 
   const {
     data: posts,
-    isLoading,
+    isPending,
     error,
   } = useQuery({
     queryKey: ["posts"],
     queryFn: () =>
-      fetch("http://localhost:3001/posts").then((res) => res.json()),
+      fetch("http://localhost:3001/posts").then(
+        (res) => res.json() as Promise<Post[]>
+      ),
   });
 
   // useEffect(() => {
@@ -28,7 +30,7 @@ const HomePage = () => {
   //     .then((data) => setPosts(data)); // setPosts is using to set the data to the state.
   // }, []); // useEffect is using to fetch data from the server.
 
-  if (isLoading) return <p>Loading...</p>;
+  if (isPending) return <p>Loading...</p>;
   if (error) return <p>Something went wrong!</p>;
 
   // const handleDelete = (id: number) => {
@@ -44,22 +46,22 @@ const HomePage = () => {
     <div className="min-h-screen flex bg-gradient-to-r from-[#8eaad4] to-[#ddb2bc] px-2">
       <div>
         <h1 className="text-2xl font-bold py-2 text-center">Post Page</h1>
-        {posts.map(() => (
+        {posts.map((post) => (
           <div
-            key={posts.id}
-            className="rounded-lg px-2 py-2 my-2 drop-shadow-md bg-rose-100 w-full"
+            key={post.id}
+            className="rounded-lg px-2 py-2 my-2 drop-shadow-md bg-rose-100 w-full md:w-3/5"
           >
-            <h2 className="font-bold text-xl">{posts.title}</h2>
-            <p>{posts.content}</p>
-            <div className="flex py-2">
+            <h2 className="font-bold text-xl">{post.title}</h2>
+            <p className="py-4">{post.content}</p>
+            <div className="flex">
               <button
-                onClick={() => navigate("/edit/" + posts.id)}
-                className="px-4 py-2 bg-amber-300 flex items-center rounded hover:bg-amber-400 "
+                onClick={() => navigate("/edit/" + post.id)}
+                className="px-4 py-2 bg-amber-300 flex items-center rounded right-2 hover:bg-amber-400 "
               >
                 Edit <span className="i-mdi-file-edit" />
               </button>
               <button
-                className="ml-2 px-4 py-2 bg-red-300 flex items-center rounded hover:bg-red-400 "
+                className="ml-2 px-4 py-2 bg-red-300 flex items-center rounded right-2 hover:bg-red-400 "
                 // onClick={() => handleDelete(post.id)} // example of using handleDelete function.
               >
                 Delete <span className="i-mdi-delete" />
